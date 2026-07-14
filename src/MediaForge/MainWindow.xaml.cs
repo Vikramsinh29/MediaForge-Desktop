999567f77e9d275bf9604a16e05cad31a14d5706
Vikramsinh <vikramsinh29@gmail.com>
2026-07-14 15:55:54 +0530

    checkpoint: sprint 3.3 foundation

--- src/MediaForge/MainWindow.xaml.cs
+++ src/MediaForge/MainWindow.xaml.cs
@@ -1,6 +1,4 @@
-﻿using System.IO;
-using System.Windows;
-using Microsoft.Win32;
+﻿using System.Windows;
 
 namespace MediaForge;
 
@@ -9,31 +7,5 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
-    }
-
-    private void OpenMedia_Click(object sender, RoutedEventArgs e)
-    {
-        OpenFileDialog dialog = new()
-        {
-            Title = "Open Media",
-            Filter =
-                "Media Files|*.mp4;*.mkv;*.avi;*.mov;*.mp3;*.wav;*.flac;*.aac|All Files|*.*"
-        };
-
-        if (dialog.ShowDialog() != true)
-            return;
-
-        FileInfo file = new(dialog.FileName);
-
-        FileNameBox.Text = file.Name;
-        FilePathBox.Text = file.FullName;
-        FileSizeBox.Text = $"{file.Length:N0} bytes";
-
-        StatusText.Text = "Media Loaded";
-    }
-
-    private void Exit_Click(object sender, RoutedEventArgs e)
-    {
-        Close();
     }
 }
