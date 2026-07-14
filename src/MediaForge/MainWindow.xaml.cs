43dcc71591a876f24d6ba9e8e3c867053812057e
Vikramsinh <vikramsinh29@gmail.com>
2026-07-14 15:36:37 +0530

    feat: open media file

--- src/MediaForge/MainWindow.xaml.cs
+++ src/MediaForge/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
-﻿using System.Windows;
-using MediaForge.ViewModels;
+﻿using System.IO;
+using System.Windows;
+using Microsoft.Win32;
 
 namespace MediaForge;
 
@@ -8,6 +9,31 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
-        DataContext = new MainWindowViewModel();
+    }
+
+    private void OpenMedia_Click(object sender, RoutedEventArgs e)
+    {
+        OpenFileDialog dialog = new()
+        {
+            Title = "Open Media",
+            Filter =
+                "Media Files|*.mp4;*.mkv;*.avi;*.mov;*.mp3;*.wav;*.flac;*.aac|All Files|*.*"
+        };
+
+        if (dialog.ShowDialog() != true)
+            return;
+
+        FileInfo file = new(dialog.FileName);
+
+        FileNameBox.Text = file.Name;
+        FilePathBox.Text = file.FullName;
+        FileSizeBox.Text = $"{file.Length:N0} bytes";
+
+        StatusText.Text = "Media Loaded";
+    }
+
+    private void Exit_Click(object sender, RoutedEventArgs e)
+    {
+        Close();
     }
 }
